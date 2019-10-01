bd43a2474ca1c03cff970c6c5875fa06893171c5
Ziyad AlQahtani <ziyadmsq@gmail.com>
2019-10-01 23:50:57 +0300

    init

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=rishizek/machine-learning-microservice:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment machine-learning-microservice --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward doployment/machine-learning-microservice 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=ziyadmsq/machine-learning-microservice:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login ziyadmsq
+docker tag machine-learning-microservice $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker puch $dockerpath
